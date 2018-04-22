0ef72cbdba331a913c303961b057b7325a3ac56e
Sid Nutulapati <sidgonuts@gmail.com>
2018-04-22 16:45:11 -0400

    get panorama from vid working

--- video_panorama.py
+++ video_panorama.py
@@ -7,8 +7,10 @@ Sid Nutulapati
 import argparse
 import cv2
 import imutils
+import numpy as np
+import matplotlib.pyplot as plt
 
-SAMPLE_RATE = 10
+SAMPLE_RATE = 15
 
 
 def main():
@@ -23,8 +25,10 @@ def main():
   while True:
     success, image = vidcap.read()
     if success and count % SAMPLE_RATE == 0:
-      success, image = vidcap.read()
-      frames.append(image)
+      rows, cols, _ = image.shape
+      M = cv2.getRotationMatrix2D((cols/2, rows/2), 270, 1)
+      dest = cv2.warpAffine(image, M, (cols, rows))
+      frames.append(dest)
       print('Read a new frame: ', success)
     count += 1
     if success == False:
@@ -35,17 +39,26 @@ def main():
   cv2.ocl.setUseOpenCL(False)
   stitcher = cv2.createStitcher(False)
 
-
   print("Trying to use the cv2 stitcher...")
   result = stitcher.stitch(frames)
-
+  # plot('Frame something', imutils.resize(frames[2], height=500))
   if result[1] is not None:
     print("Success!")
-    cv2.imshow('Result', imutils.resize(result[1], height=500))
+    cv2.imshow("Result w/ Sample Rate {}".format(SAMPLE_RATE), imutils.resize(result[1], height=500))
     cv2.waitKey(0)
   else:
     print("Failure :(")
     return
+  # plt.show()
+  return
+
+
+def plot(title, image):
+  plt.figure(title)
+  plt.axis("off")
+  plt.imshow(image)
+  plt.draw()
+
 
 
 if __name__ == "__main__":
